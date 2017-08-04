6888604491153b8cecc0660170b9188887c71d5d
AlexanderC <alexander.moldova@gmail.com>
2017-08-04 18:19:59 +0300

    Generate API docs

--- docs/api/script/search_index.js
+++ docs/api/script/search_index.js
@@ -288,6 +288,12 @@ window.esdocSearchIndex = [
     "function"
   ],
   [
+    "recink/src/component/e2e/testcafe/nightmare-browser-provider-registrar.js~register",
+    "function/index.html#static-function-register",
+    "<span>register</span> <span class=\"search-result-import-path\">recink/src/component/e2e/testcafe/nightmare-browser-provider-registrar.js</span>",
+    "function"
+  ],
+  [
     "builtinexternal/ecmascriptexternal.js~array",
     "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
     "BuiltinExternal/ECMAScriptExternal.js~Array",
@@ -1215,6 +1221,12 @@ window.esdocSearchIndex = [
     "src/component/e2e/events.js",
     "file/src/component/e2e/events.js.html",
     "src/component/e2e/events.js",
+    "file"
+  ],
+  [
+    "src/component/e2e/testcafe/nightmare-browser-provider-registrar.js",
+    "file/src/component/e2e/testcafe/nightmare-browser-provider-registrar.js.html",
+    "src/component/e2e/testcafe/nightmare-browser-provider-registrar.js",
     "file"
   ],
   [
