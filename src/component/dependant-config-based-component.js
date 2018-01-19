5c0d88a3dd5240d5e591cdebd1ed1c9640dd4ce2
ddimitrioglo <ddimitrioglo@pycoding.biz>
2018-01-20 00:16:28 +0200

    #RECINK Integrate tests runner into run terraform

--- src/component/dependant-config-based-component.js
+++ src/component/dependant-config-based-component.js
@@ -27,50 +27,44 @@ class DependantConfigBasedComponent extends ConfigBasedComponent {
    * @returns {Promise}
    */
   waitConfig(emitter) {
-    return super.waitConfig(emitter)
-      .then(container => {
-        if (container) {
-          if (this.dependencies.length <= 0) {
-            return Promise.resolve(container);
-          }
-          
-          const waitComponents = this.dependencies.map(name => {
-            const component = emitter.component(name);
-            
-            if (!component) {
-              return Promise.resolve({ name, isActive: false });
-            }
-            
-            return component.ready()
-              .then(() => Promise.resolve(component));
-          });
-          
-          return Promise.all(waitComponents)
-            .then(components => {
-              let failedComponents = [];
-              
-              components.map(component => {
-                if (!component.isActive) {
-                  failedComponents.push(component.name);
-                }
-              });
-              
-              if (failedComponents.length > 0) {
-                this.setActive(false);
-                
-                const failedInfo = failedComponents.map(c => chalk.red(c)).join(', ');
-                
-                this.logger.info(
-                  `${ this.logger.emoji.fire } ${ this.name } ~ ${ failedInfo }`
-                );
-              }
-              
-              return Promise.resolve(failedComponents.length <= 0 ? container : null);
-            });
+    return super.waitConfig(emitter).then(container => {
+      if (container) {
+        if (this.dependencies.length <= 0) {
+          return Promise.resolve(container);
         }
-        
-        return Promise.resolve(null);
-      });
+
+        const waitComponents = this.dependencies.map(name => {
+          const component = emitter.component(name);
+
+          if (!component) {
+            return Promise.resolve({ name, isActive: false });
+          }
+
+          return component.ready().then(() => Promise.resolve(component));
+        });
+
+        return Promise.all(waitComponents).then(components => {
+          let failedComponents = [];
+
+          components.map(component => {
+            if (!component.isActive) {
+              failedComponents.push(component.name);
+            }
+          });
+
+          if (failedComponents.length > 0) {
+            const failedInfo = failedComponents.map(c => chalk.red(c)).join(', ');
+
+            this.setActive(false);
+            this.logger.info(`${ this.logger.emoji.fire } ${ this.name } ~ ${ failedInfo }`);
+          }
+
+          return Promise.resolve(failedComponents.length <= 0 ? container : null);
+        });
+      }
+
+      return Promise.resolve(null);
+    });
   }
 }
 
